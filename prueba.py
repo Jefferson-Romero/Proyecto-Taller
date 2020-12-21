77ceb5381597c1a2185c503710eb1a1c4b17abe3
Jefferson Romero <jromeroc8@est.ups.edu.ec>
2020-12-20 23:11:57 -0500

    Commit inicial

---
+++ prueba.py
@@ -0,0 +1,10 @@
+# Script de prueba para Git
+# Autor: Jefferson Romero <jromeroc8@est.ups.edu.ec> 
+# Fecha: 2020-12-20
+# Version: 1.0.0
+
+def main():
+    print("Hola Mundo!")
+
+if __name__ == "__main__":
+    main()
